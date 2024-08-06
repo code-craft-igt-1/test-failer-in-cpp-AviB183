6f6ec50d92d859ccba9805c4cdadd9470fbe3f98
Avinash Bhaskar Bhat <AvinashBhaskar.Bhat@philips.com>
2024-08-06 19:52:30 +0530

    use of explicit std:: (pipeline check)

--- misaligned.cpp
+++ misaligned.cpp
@@ -3,7 +3,6 @@
 #include <sstream>
 #include <vector>
 #include <string>
-using namespace std;
 
 const char* majorColor[] = { "White", "Red", "Black", "Yellow", "Violet" };
 const char* minorColor[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
@@ -13,13 +12,13 @@ int printColorMap()
     int i = 0, j = 0;
     for (i = 0; i < 5; i++) {
         for (j = 0; j < 5; j++) {
-            cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
         }
     }
     return i * j;
 }
 
-static void _convertStrStreamToStrList(stringstream strStream, vector<string> &strList)
+static void _convertStrStreamToStrList(std::stringstream strStream, std::vector<std::string> &strList)
 {
     strList.clear();
 
@@ -31,41 +30,41 @@ static void _convertStrStreamToStrList(stringstream strStream, vector<string> &s
     }
 }
 
-static void _getColorMapOutputLines(vector <string>& outputLines)
+static void _getColorMapOutputLines(std::vector <std::string>& outputLines)
 {
-    ostringstream outputStream;
-    streambuf* coutBuffBackup = cout.rdbuf();
-    cout.rdbuf(outputStream.rdbuf());
+    std::ostringstream outputStream;
+    std::streambuf* coutBuffBackup = std::cout.rdbuf();
+    std::cout.rdbuf(outputStream.rdbuf());
 
     int result = printColorMap();
 
-    cout.rdbuf(coutBuffBackup);
+    std::cout.rdbuf(coutBuffBackup);
 
-    _convertStrStreamToStrList(stringstream(outputStream.str()), outputLines);
+    _convertStrStreamToStrList(std::stringstream(outputStream.str()), outputLines);
 }
 
-static bool _checkContents(const vector <string> &outputList)
+static bool _checkContents(const std::vector <std::string> &outputList)
 {
     bool contentPresent = true;
     int listIndex = 0;
 	for (int i = 0; i < 5 && contentPresent; ++i)
         for (int j = 0; j < 5 && contentPresent; ++j)
         {
-            contentPresent = (outputList[listIndex].find(majorColor[i]) != string::npos &&
-                outputList[listIndex].find(minorColor[j]) != string::npos);
+            contentPresent = (outputList[listIndex].find(majorColor[i]) != std::string::npos &&
+                outputList[listIndex].find(minorColor[j]) != std::string::npos);
             ++listIndex;
         }
 
     return contentPresent;
 }
 
-static bool _checkFormat(const vector <string>& outputList, const char& delim)
+static bool _checkFormat(const std::vector <std::string>& outputList, const char& delim)
 {
     bool formatIsCorrect = true;
 
     size_t pos1 = outputList[0].find(delim);
     size_t pos2 = outputList[0].find(delim, pos1 + 1);
-    formatIsCorrect = (pos1 != string::npos && pos2 != string::npos);
+    formatIsCorrect = (pos1 != std::string::npos && pos2 != std::string::npos);
 
     for (int i = 0; i < outputList.size() && formatIsCorrect; ++i)
         formatIsCorrect = (outputList[i][pos1] == delim && outputList[i][pos2] == delim);
@@ -75,13 +74,13 @@ static bool _checkFormat(const vector <string>& outputList, const char& delim)
 
 int main()
 {
-    vector <string> outputLines;
+    std::vector <std::string> outputLines;
     _getColorMapOutputLines(outputLines);
 
     assert(outputLines.size() == 25);
     assert(_checkContents(outputLines) == true);
     assert(_checkFormat(outputLines, '|') == true);
     
-    cout << "All is well (maybe!)\n";
+    std::cout << "All is well (maybe!)\n";
     return 0;
 }
